bc1dd280d28571bd1fabbb47e16b0285f44afc63
ashish choudhary <achoudhary94@gmail.com>
2019-11-10 15:47:22 -0800

    Dashboard changes

--- imports/ui/pages/Profile/components/ProfilePage.js
+++ imports/ui/pages/Profile/components/ProfilePage.js
@@ -5,7 +5,12 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { USER_TYPE } from '../../../../constants';
+import Chart from 'react-google-charts';
+import Divider from '@material-ui/core/Divider';
+import Issues from '../../../../api/issues/issues';
+import { withTracker } from 'meteor/react-meteor-data';
+import Spinner from '../../../components/Spinner';
+import { USER_TYPE, ISSUE_STATE } from '../../../../constants';
 
 const useStyles = makeStyles({
   card: {
@@ -16,7 +21,19 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MediaCard() {
+const options = {
+  title: 'Status of Raised Issues',
+  pieHole: 0.4,
+  is3D: true,
+};
+
+function MediaCard({
+  propsReady,
+  backlogCount,
+  todoCount,
+  inProgressCount,
+  completedCount,
+}) {
   const classes = useStyles();
   const userName = Meteor.user().name;
   const emailId = 'Email - ' + Meteor.user().emails[0].address;
@@ -25,36 +42,86 @@ export default function MediaCard() {
     'Address - ' + Meteor.user().address + ', Zip - ' + Meteor.user().zip;
   const designation = 'Designation - ' + Meteor.user().designation;
 
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/imports/ui/pages/Profile/avatr.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {userName}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {address}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {emailId}
-          </Typography>
-          {userType === USER_TYPE.REPRESENTATIVE.id && (
-            <div className="rep-fields">
+  const data = [
+    ['Task', 'Hours per Day'],
+    ['In Progress', inProgressCount],
+    ['To-Do', todoCount],
+    ['Backlogged', backlogCount],
+    ['Completed', completedCount],
+  ];
+
+  console.log('Backlog count ' + backlogCount);
+
+  return !propsReady ? (
+    <Spinner />
+  ) : (
+    <React.Fragment>
+      <Card className={classes.card}>
+        <CardActionArea>
+          {/* <CardMedia
+            className={classes.media}
+            image=""
+            title="Contemplative Reptile"
+          /> */}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {userName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {address}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {emailId}
+            </Typography>
+            {userType === USER_TYPE.REPRESENTATIVE.id && (
               <Typography variant="body2" color="textSecondary" component="p">
                 User Type - {userType}
               </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {designation}
-              </Typography>
-            </div>
-          )}
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            )}
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Divider />
+      {userType === USER_TYPE.REPRESENTATIVE.id && (
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          graph_id="PieChart"
+          width={'100%'}
+          height={'400px'}
+          legend_toggle
+        />
+      )}
+    </React.Fragment>
   );
 }
+
+export default withTracker(() => {
+  const subscriberHandles = [Meteor.subscribe('issues.stateCount')];
+
+  const propsReady = subscriberHandles.every(handle => handle.ready());
+
+  let backlogCount = 0;
+  let todoCount = 0;
+  let inProgressCount = 0;
+  let completedCount = 0;
+
+  if (propsReady) {
+    backlogCount = Issues.find({ state: ISSUE_STATE.BACKLOG.id }).count();
+    todoCount = Issues.find({ state: ISSUE_STATE.TODO.id }).count();
+    inProgressCount = Issues.find({ state: ISSUE_STATE.INPROGRESS.id }).count();
+    completedCount = Issues.find({ state: ISSUE_STATE.DONE.id }).count();
+  }
+
+  return {
+    // remote example (if using ddp)
+    // usersReady,
+    // users,
+    propsReady,
+    backlogCount,
+    todoCount,
+    inProgressCount,
+    completedCount,
+  };
+})(MediaCard);
